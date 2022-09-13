09b75a55f3850d1cfbe0770a7538833e0e7c0e9d
kishidads <kishida.ds@gmail.com>
2022-09-13 19:22:04 -0300

    Criação dos Fragments

---
+++ app/src/main/java/br/com/etecia/atividade_3hpam2_13092022/fragmentTea.java
@@ -0,0 +1,20 @@
+package br.com.etecia.atividade_3hpam2_13092022;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class fragmentTea extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_tea, container, false);
+
+        return view;
+    }
+}
