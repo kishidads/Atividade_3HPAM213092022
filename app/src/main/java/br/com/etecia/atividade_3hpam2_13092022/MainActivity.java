1ba7782e8c072d71ddd168a372ba57af3a49b40c
kishidads <kishida.ds@gmail.com>
2022-09-13 19:08:10 -0300

    Commit inicial

---
+++ app/src/main/java/br/com/etecia/atividade_3hpam2_13092022/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.atividade_3hpam2_13092022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
