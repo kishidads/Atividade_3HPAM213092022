cf0514d0a1cb91c2bdd70860af3d261284eee0b1
kishidads <kishida.ds@gmail.com>
2022-09-13 21:28:13 -0300

    Criação do menu

--- app/src/main/java/br/com/etecia/atividade_3hpam2_13092022/fragmentDonuts.java
+++ app/src/main/java/br/com/etecia/atividade_3hpam2_13092022/fragmentDonuts.java
@@ -8,7 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class FragmentDonuts extends Fragment {
+public class fragmentDonuts extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
