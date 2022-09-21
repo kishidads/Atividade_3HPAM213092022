d25c93d2aababa7f3a777441b3804cac1bdb0c6d
kishidads <kishida.ds@gmail.com>
2022-09-20 22:04:18 -0300

    Ajustes em aula.

---
+++ app/src/main/java/br/com/etecia/atividade_3hpam2_13092022/Cake_Activity.java
@@ -0,0 +1,48 @@
+package br.com.etecia.atividade_3hpam2_13092022;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class Cake_Activity extends AppCompatActivity {
+
+    private TextView txtTitulo, txtDescricao, txtCategoria;
+    private ImageView imgCake;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cake_card);
+
+        txtTitulo = findViewById(R.id.txtIdTitulo);
+        txtDescricao = findViewById(R.id.txtIdDescricao);
+        txtCategoria = findViewById(R.id.txtIdCategoria);
+        imgCake = findViewById(R.id.idImgLivroN);
+
+        //Intent que irá receber os valores da outra janela.
+        Intent intent = getIntent();
+
+        //As variáveis que irão receber os valores da outra janela.
+        String titulo, descricao, categoria;
+        int miniatura;
+
+        titulo = intent.getExtras().getString("Titulo");
+        descricao = intent.getExtras().getString("Descricao");
+        categoria = intent.getExtras().getString("Categoria");
+        miniatura = intent.getExtras().getInt("Miniatura");
+
+        txtTitulo.setText(titulo);
+        txtDescricao.setText(descricao);
+        txtCategoria.setText(categoria);
+        imgCake.setImageResource(miniatura);
+
+    }
+}
--- app/src/main/java/br/com/etecia/atividade_3hpam2_13092022/JuiceFragment.java
+++ app/src/main/java/br/com/etecia/atividade_3hpam2_13092022/JuiceFragment.java
@@ -3,10 +3,15 @@ package br.com.etecia.atividade_3hpam2_13092022;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JuiceFragment extends Fragment {
     @Override
@@ -16,5 +21,37 @@ public class JuiceFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_juice, container, false);
 
         return view;
+
+        List<Juice> id_recyclerViewJuice;
+
+            //Inserindo os livros no arrayList vazio
+            id_recyclerViewJuice = new ArrayList<>();
+
+            id_recyclerViewJuice.add(new Juice("Teste", "Teste", "Teste", R.drawable.img_juice));
+            id_recyclerViewJuice.add(new Juice("Teste", "Teste", "Teste", R.drawable.img_juice));
+            id_recyclerViewJuice.add(new Juice("Teste", "Teste", "Teste", R.drawable.img_juice));
+            id_recyclerViewJuice.add(new Juice("Teste", "Teste", "Teste", R.drawable.img_juice));
+            id_recyclerViewJuice.add(new Juice("Teste", "Teste", "Teste", R.drawable.img_juice));
+            id_recyclerViewJuice.add(new Juice("Teste", "Teste", "Teste", R.drawable.img_juice));
+
+            //declarando a variavel xml enviando para o java
+            RecyclerView mRecyclerView = findViewById(R.id.id_recyclerViewJuice);
+
+            //Instânciando o adaptador com os valores necessários
+            RecyclerViewAdapter mAdapter = new RecyclerViewAdapter(getApplicationContext(), id_recyclerViewJuice);
+            //Criando o layout para inserção dos valores
+
+            //LayoutManager não é necessário inserção de colunas - pode ser utilizado na vertical ou horizontal
+            // mRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext(), RecyclerView.HORIZONTAL,true));
+
+            //GriLayoutManager necessário a inserção de colunas
+            mRecyclerView.setLayoutManager(new GridLayoutManager(getApplicationContext(), 2));
+
+            //Para melhorar a performance do RecyclerView na listagem com um tamanho fixo
+            mRecyclerView.setHasFixedSize(true);
+
+            //Inserindo os valores na lista do RecyclerView
+            mRecyclerView.setAdapter(mAdapter);
+
     }
 }
